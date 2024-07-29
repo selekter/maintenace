b34663cfdfc4587a4accf33c54913be74915cc5d
Selekter <camono96@gmail.com>
2024-07-29 22:34:05 +0700

    commit แรก

---
+++ app/Http/Controllers/DriverController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Driver;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class DriverController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return Inertia::render('Dashboard/Driver/CreateDriver');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $driver = new Driver();
+        $driver->name = $request->driverName;
+        $driver->save();
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ resources/js/Pages/Dashboard/Driver/CreateDriver.jsx
@@ -0,0 +1,55 @@
+import Authenticated from "@/Layouts/AuthenticatedLayout";
+import { Head, useForm } from "@inertiajs/react";
+
+export default function CreateDriver({ auth }) {
+  const { data, setData, post, processing, errors, reset } = useForm({
+    driverName: "",
+  });
+
+  function submit(e) {
+    e.preventDefault();
+
+    post(route("driver.store"), {
+      preserveScroll: true,
+      onSuccess: () => reset(),
+    });
+    // setData("driverName", "");
+  }
+  return (
+    <Authenticated user={auth.user} header="Create Driver">
+      <Head title="Create Driver" />
+      <div className="py-12">
+        <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+          <div className="bg-white shadow-sm sm:rounded-lg">
+            <div className="p-6 text-gray-900">
+              CreateDriver
+              <form onSubmit={submit}>
+                <div className="py-3">
+                  <label htmlFor="driverName">Driver Name</label>
+                  <input
+                    id="driverName"
+                    type="text"
+                    className="w-full rounded-md shadow-sm border-gray-300 focus:border-indigo-500 focus:ring-indigo-500"
+                    value={data.driverName}
+                    onChange={(e) => setData("driverName", e.target.value)}
+                    autoFocus
+                  />
+                  {errors.driverName && <div>{errors.driverName}</div>}
+                </div>
+                <div className="flex flex-col md:flex-row">
+                  <button
+                    type="submit"
+                    className="bg-blue-500 text-white px-3 py-1 rounded"
+                    disabled={processing}
+                  >
+                    Submit
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Authenticated>
+  );
+}
